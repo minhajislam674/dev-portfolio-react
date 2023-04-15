040833a570a32fa9e4f78424a4bef417f4ca7507
Minhaj Islam <minhajislam674@gmail.com>
2023-04-15 20:46:27 +0200

    Third iteration of the site - changes in layout and new project has been added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
-  mode: 'jit',
+  mode: "jit",
   theme: {
     extend: {
       colors: {
-        'olive-light': '#94B0B3',
-        'blue': '#7ED6DF',
-        'blue-dark': '#449FA8',
-        'olive': '#324B4E',
-        'purple': '#7e5bef',
-        'pink': '#ff49db',
-        'orange': '#ff7849',
-        'green': '#13ce66',
-        'yellow': '#ffc82c',
-        'gray-dark': '#273444',
-        'gray': '#8492a6',
-        'gray-light': '#d3dce6',
-        'bg-dark':'#002B34',
-        'light':'#dff9fb',
+        "olive-light": "#2E4F4F",
+        blue: "#7ED6DF",
+        "blue-dark": "#0E8388",
+        olive: "#324B4E",
+        purple: "#7e5bef",
+        pink: "#ff49db",
+        orange: "#ff7849",
+        green: "#13ce66",
+        yellow: "#ffc82c",
+        "gray-dark": "#273444",
+        gray: "#8492a6",
+        "gray-light": "#d3dce6",
+        "bg-dark": "#2C3333",
+        light: "#dff9fb",
       },
       backgroundImage: (theme) => ({
         "gradient-rainbow":
@@ -27,25 +27,24 @@ module.exports = {
         "gradient-rainblue":
           "linear-gradient(90deg, #24CBFF 14.53%, #FC59FF 69.36%, #FFBD0C 117.73%)",
       }),
-    fontFamily: {
-      opensans: ["Open Sans", "sans-serif"],
-      roboto: ["Roboto", "sans-serif"],
-      montserrat: ["Montserrat", "sans-serif"],
-      poppins: ["Poppins", "sans-serif"],
-      lato: ["Lato", "sans-serif"],
+      fontFamily: {
+        opensans: ["Open Sans", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+        lato: ["Lato", "sans-serif"],
+      },
+
+      screens: {
+        xs: "320px",
+        sm: "640px",
+        md: "768px",
+        xmd: "960px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
     },
-    
-    screens: {
-      'xs': '320px',
-      'sm': '640px',
-      'md': '768px',
-      'xmd': '960px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px',
-      
+    plugins: [],
   },
-  },
-  plugins: [],
-}
-}
+};
